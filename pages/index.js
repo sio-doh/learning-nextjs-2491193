66743c890a2898ebaf1cb776527f3f835b67052f
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-05-09 15:01:35 +0200

    included dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css' 
 import Link from 'next/link'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,95 +11,45 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <p>Welcome to my eLearning Platform</p>
         </div>
 
         <div className={styles.grid}>
-          <Link href="/learn/nextjs">
-            <h2 className={inter.className}>
-              Learn NextJS <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
+          <Link href="/learn/next" className={styles.card}>
+            <h2>Learn NextJS <span>-&gt;</span></h2>
+            <p>Find in-depth information about Next.js features and&nbsp;API.</p>
           </Link>
-          <Link href="/learn/react">
-            <h2 className={inter.className}>
-              Learn React<span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about React.js in an interactive course with&nbsp;quizzes!
-            </p>
+
+          <Link href="/learn/react" className={styles.card}>
+            <div>
+              <h2>Learn React<span>-&gt;</span></h2>
+              <p>Learn about React.js in an interactive course with&nbsp;quizzes!</p>
+            </div>
           </Link>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+
+          <Link href="/learn/vue" className={styles.card}>
+            <div>
+              <h2>Learn Vue<span>-&gt;</span></h2>
+              <p>Learn about Vue.js in an interactive course with&nbsp;quizzes!</p>
+            </div>
+          </Link>
+
+          <Link href="/learn/angular" className={styles.card}>
+            <div>
+              <h2>Learn Angular<span>-&gt;</span></h2>
+              <p>Learn about Angular.js in an interactive course with&nbsp;quizzes!</p>
+            </div>
+          </Link>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
+            <h2>Deploy <span>-&gt;</span></h2>
+            <p>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.</p>
           </a>
         </div>
       </main>
