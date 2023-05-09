099e4b118ce26746658500382f2b8de9cf060f0f
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-05-09 13:18:49 +0200

    created new page

---
+++ pages/hello.js
@@ -0,0 +1,8 @@
+export default function Hello() {
+    return (
+        <>
+            <h1>Learning NextJS</h1>
+            <p>Suspendisse tincidunt rutrum ex ac ultrices. Nulla urna ex, elementum nec rhoncus eget, lobortis vel orci. Donec aliquet nec risus et fringilla. Cras vel est lacinia sem pharetra ultricies tempor sit amet massa. Quisque varius dolor a mollis vehicula. Duis tempor malesuada nisl, et faucibus elit vehicula et. Vivamus massa urna, eleifend ac nisl eu, porttitor euismod elit. Nullam non sem eu lectus dapibus suscipit. Aenean aliquet eu massa ac volutpat. Suspendisse pharetra iaculis efficitur. Mauris sit amet est quis ligula dictum suscipit. Cras sit amet efficitur risus. Fusce non euismod libero.</p> 
+        </>
+    )
+}
