79ab18fa77cee71f122434bb92adf29d7425c889
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-05-10 11:00:24 +0200

    folder restructuring

--- 2_getting_started/pages/index.js
+++ 2_getting_started/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css' 
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -54,5 +54,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
-}
+  );
+};
